3e36ab9a008353e9ee4a8db6a5abb6c243f90e8c
Viktor Zinoviev <vilenet607@gmail.com>
2024-10-23 16:57:05 +0100

    upd

--- cpp-py/main.cpp
+++ cpp-py/main.cpp
@@ -5,9 +5,6 @@
 
 int main()
 {
-    SetEnvironmentVariable("PYTHONHOME", "C:\\msys64\\mingw64");
-    SetEnvironmentVariable("PYTHONPATH", "C:\\msys64\\mingw64\\lib");
-
     Py_Initialize();
 
     PyRun_SimpleString("print('Hello from Python')");
